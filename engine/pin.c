7af39fa19c99ff11dd2add1b2faed47e78e6b88e
kwuerch <kwuerch@calpoly.edu>
2017-01-01 14:53:59 -0800

    Added outline for detecting absolute pins

---
+++ engine/pin.c
@@ -0,0 +1,14 @@
+#include <config.h>
+#include <board.h>
+#include <mask.h>
+
+void getAbsolutePins(board *b, Color c){
+    if(c == WHITE){
+        // Check Full Board Diagonal with black bishops and Queens -- Then and to from with white pieces
+        // Check Full Board Horizontal and Verticals with Black Rooks and Queens -- Then and to from with white piece
+
+    }else if(c == BLACK){
+        // Same as c == white, but oposite on the colors
+    }
+
+}
