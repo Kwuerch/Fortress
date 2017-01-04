9bf0f141c5bd77433587d720f1c488a31bb9cd01
kwuerch <kwuerch@calpoly.edu>
2017-01-03 20:44:57 -0800

    Fixed error in knight move mask created from update of Board Rep

--- engine/mask.c
+++ engine/mask.c
@@ -152,18 +152,18 @@ void initPawnMask(){
 void initKngtMask(){
     for(int i = 0; i < 64; i++){
         uint64_t index = indxMask[i];
-        piecMask[KNIGHT][i] |= ((index <<  6) | (index >> 10)) & notGHFile;
-        piecMask[KNIGHT][i] |= ((index << 15) | (index >> 17)) & notHFile;
-        piecMask[KNIGHT][i] |= ((index << 17) | (index >> 15)) & notAFile;
-        piecMask[KNIGHT][i] |= ((index << 10) | (index >>  6)) & notABFile;
+        piecMask[KNIGHT][i] |= ((index <<  6) | (index >> 10)) & notABFile;
+        piecMask[KNIGHT][i] |= ((index << 15) | (index >> 17)) & notAFile;
+        piecMask[KNIGHT][i] |= ((index << 17) | (index >> 15)) & notHFile;
+        piecMask[KNIGHT][i] |= ((index << 10) | (index >>  6)) & notGHFile;
     }
 }
 
 void initKingMask(){
     for(int i = 0; i < 64; i++){
         uint64_t index = indxMask[i];
-        piecMask[KING][i] |= ((index << 7) | (index >> 1) | (index >> 9)) & notHFile;
-        piecMask[KING][i] |= ((index >> 7) | (index << 1) | (index << 9)) & notAFile;
+        piecMask[KING][i] |= ((index << 7) | (index >> 1) | (index >> 9)) & notAFile;
+        piecMask[KING][i] |= ((index >> 7) | (index << 1) | (index << 9)) & notHFile;
         piecMask[KING][i] |= (index << 8) | (index >> 8);
     }
 }
